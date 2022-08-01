72cf3093ed3dde8492e450490a978ee231bd3531
jiyabra <86106526+jiyabra@users.noreply.github.com>
2022-08-01 03:05:32 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function setup()
+{
+    canvas = createCanvas(380 , 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380 , 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd' , modalLoaded);
+    document.getElementById("status").innerHTML = "status-detecting objects";
+}
+
+function modalLoaded()
+{
+    console.log("Modal is Loaded!");
+    status = true;
+    
+}
+
+function gotResult(error , results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+
+}
+
+status = "";
+img = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+function draw()
+{
+  image(video , 0 , 0 , 380 , 380);
+  if(status != "")
+  {
+      r = random(255);
+      g = random(255);
+      b = random(255);
+
+      objectDetector.detect(video , gotResult);
+      for(i = 0; i < objects.length; i++)
+      {
+          document.getElementById("status").innerHTML = "status : object detected";
+          document.getElementById("number_of_detection").innerHTML = "number of object detected are : " + objects.length;
+          fill(r,g,b);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + "" + percent + "%" , objects[i].x , objects[i].y);
+          noFill();
+          stroke(r,g,b);
+          rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+      }
+  }
+}
